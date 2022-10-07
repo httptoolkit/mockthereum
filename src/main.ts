2cde834217f265631e7383c963d2eeba09214883
Tim Perry <pimterry@gmail.com>
2022-10-07 16:42:38 +0200

    Build reference TypeDoc in CI

--- src/main.ts
+++ src/main.ts
@@ -17,3 +17,11 @@ export function getRemote(options?: Mockttp.MockttpOptions & MockthereumOptions)
 export function getAdminServer(options?: Mockttp.MockttpAdminServerOptions) {
     return Mockttp.getAdminServer(options);
 }
+
+// Export various internal types:
+export type { MockthereumNode, MockthereumOptions };
+export type { CallRuleBuilder, TransactionRuleBuilder } from './contract-rule-builder';
+export type { BalanceRuleBuilder, BlockNumberRuleBuilder, GasPriceRuleBuilder } from './single-value-rule-builders';
+export type { MockedContract } from './mocked-contract';
+export type { RpcErrorProperties } from './jsonrpc';
+export type { RawTransactionReceipt } from './mock-node';
